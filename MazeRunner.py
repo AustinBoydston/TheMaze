b166ebc28dca62ad8105fcc5b0da1e13e8b723d0
Austin Boydston <43730698+AustinBoydston@users.noreply.github.com>
2024-03-28 17:20:45 -0500

    Update MazeRunner.py
    updated

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,5 +1,8 @@
 import os
+import random
 
+MazeLayoutData = []
+Length = 0
 
 print("_________          _______    _______  _______  _______  _______ ")
 print("\\__   __/|\\     /|(  ____ \\  (       )(  ___  )/ ___   )(  ____ \\")
@@ -25,6 +28,7 @@ print("                    4: Endless")
 while(True):
     try:   
         Difficulty = int(input())
+        CL()
         break
     except:
         pass
@@ -32,4 +36,39 @@ while(True):
 #Clear the terminal screen (just to make less typing for me)
 def CL():
     os.system('cls')
+
+
+#Create the empty maze
+def InitMazeStorage(MDataL, DifficultySetting):
+    #Temporary matrix to add to the main marix later
+    temp = []
+    
+    if DifficultySetting == 1:
+        length = random.randint(20, 40)
+    elif DifficultySetting == 2:
+        length = random.randint(60, 120)
+    elif DifficultySetting == 3:
+        length = random.randint(200, 500)
+    elif DifficultySetting == 4:
+        length = random.randint(600, 1000)
+
+    for i in range(length):
+        temp.append(0)
+    for i in range(length):
+        MDataL.append(temp)
+    return MDataL, length
+
+
+
+def GenerateMaze(DiffucultySetting):
+    return
+
+
+
+#########################################Call Methods and Run Game########################
+
+MazeLayoutData, Length = InitMazeStorage(MazeLayoutData, Difficulty)
+
+
+
 
